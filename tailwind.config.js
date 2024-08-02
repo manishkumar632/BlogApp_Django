2fb9151dc9869fe030a984f0b802e7363cd0c093
manishkumar632 <manishmu632@gmail.com>
2024-08-03 01:33:31 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/templates/**/*.html",
+        "./app/templates/includes/**/*.html"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                color1: "#e5d9f2",
+                color2: "#f5efff",
+                color3: "#cdc1ff",
+                color4: "#a594f9",
+                color5: "#7371fc",
+                color6: "#0F172A",
+                color7: "#7F92B0",
+            }
+        }
+    },
+    plugins: []
+};
+
