f856fd5894ce311cc39979e25d6e2ff2ae344823
manishkumar632 <manishmu632@gmail.com>
2024-08-03 17:14:17 +0530

    .

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -111,8 +111,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "app/static/css",
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
