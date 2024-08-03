a6d6b235117161c48120a102ae264db72faf8e7b
manishkumar632 <manishmu632@gmail.com>
2024-08-03 13:52:20 +0530

    .

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -110,7 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "app/static/css"]
+STATICFILES_DIRS = [
+    BASE_DIR / "app/static/css",
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
